65f727da11f400dcdb1eb5c0cffc4a1fc18d4c8f
lamisy93 <ami.sy@hotmail.fr>
2018-09-24 22:38:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,112 @@
+/*(function () {*/
+/*
+var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "Californie", "Caroline du Nord", "Caroline du Sud", "Colorado", "Connecticut", "Dakota du Nord", "Dakota du Sud", "Delaware", "Floride", "Géorgie", "Hawaï", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiane", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "Nouveau-Mexique", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvanie", "Rhode Island", "Tennessee", "Texas", "Utah", "Vermont", "Virginie", "Virgine-Occidentale", "Washington", "Wisconsin", "Wyoming"];
+
+*/
+/*var start = function () {
+
+
+	function myFunction() {*/
+    // Declare variables
+/*    var input, filter, ol, li, a, i;
+    input = document.getElementById('entree');
+    filter = input.value.toUpperCase();
+    ol = document.getElementById("liste");
+    li = ol.getElementsByTagName('li');*/
+
+    // Loop through all list items, and hide those who don't match the search query
+/*    for (i = 0; i < li.length; i++) {
+        a = li[i]
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+};*/
+
+  /*      for (i = 0; i < states.length; i += 1) {
+
+	        var ol = document.getElementById("liste");
+			var li = document.createElement("li");
+			ol.appendChild(li);
+
+		    var cible = states[i];y
+			li.innerHTML = `${cible}`;
+        }
+*/
+
+/*      function filtreTexte(requete) {
+      	return states.filter(function (el) {
+      		return el.toLowerCase().indexOf(requete.toLowerCase()) > -1;
+      	})
+      }
+
+      console.log(filtreTexte('al'));
+
+       filtreTexte();*/
+
+
+/*		var gererInput = function () {
+
+	    var entree = document.getElementById("entree").value;
+	    entree.addEventListener("input", function() {
+        states.filter();
+	    })
+
+		}*/
+
+        
+/*        function gererMatch() {
+
+
+            var entree = document.getElementById("entree");
+            var valeur = entree.value;*/
+           /* entree.onkeyup = ();*/
+ /*           var filtre = states.filter(function(element) {
+              return element == valeur;
+              console.log(filtre);
+              li.innerHTML = `${filtre}`;
+          });*/
+/*            var str = "Alabama Alaska Arizona Arkansas Californie Caroline du Nord Caroline du Sud Colorado Connecticut Dakota du Nord Dakota du Sud Delaware Floride Géorgie Hawaï Idaho Illinois Indiana Iowa Kansas Kentucky Louisiane Maine Maryland Massachusetts Michigan Minnesota Mississippi Missouri Montana Nebraska Nevada New Hampshire New Jersey Nouveau-Mexique New York Ohio Oklahoma Oregon Pennsylvanie Rhode Island Tennessee Texas Utah Vermont Virginie Virgine-Occidentale Washington Wisconsin Wyoming";
+            var correspondances = str.match(valeur);*/
+/*          	states.filter(function(e) {
+	        if (correspondances === str.match(valeur)) {
+		        console.log(correspondances);
+		        li.innerHTML = `${correspondances}`;    
+        };
+    });*/
+
+            /*var regexp = /[A-Z]/gi;*/
+/*
+            console.log(correspondances);*/
+/*          entree.addEventListener("keyup", compare_nom);*/
+   
+/*};
+
+
+
+	var compare_nom = function() {
+     
+    for(var i = 0; i < states.length; i++) {
+         
+        value_filter = entree.value;
+        ma_liste = states[i];
+        compare = ma_liste.indexOf(value_filter);
+    
+    }
+
+
+	entree.addEventListener("keyup", gererMatch);*/
+
+	/*  gererInput();
+    letters();*/
+/*    myFunction();*/
+/*    compare_nom();*/
+/*
+};
+
+
+window.addEventListener("DOMContentLoaded", start);
+
+}());*/
